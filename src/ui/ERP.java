d5462d79b5f212922ef3be3dcdf10f47fba1d109
aabbsi1994 <aabbsi1994@gmail.com>
2016-07-09 06:35:43 +0430

    DB Initiation debugged

--- src/ui/ERP.java
+++ src/ui/ERP.java
@@ -1,11 +1,14 @@
 package ui;
 
+import data.DBInitiation;
 import graphical_views.LoginView;
 import user_management.ManagerWrapper;
 
 public class ERP {
 	public static void main(String[] args) {
 		ManagerWrapper mw = new ManagerWrapper();
+		DBInitiation db = new DBInitiation();
+		System.out.println(db.initiate());
 		//mw.addUser("ali", "Ali", "[C@158b7f4");
 		LoginView login = new LoginView();
 		login.show(false);
