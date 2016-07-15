f6cb0e278ef02b74931a74bdb55925258e3adac3
aabbsi1994 <aabbsi1994@gmail.com>
2016-07-15 14:41:53 +0430

    adding docs

--- src/ui/ERP.java
+++ src/ui/ERP.java
@@ -5,15 +5,15 @@ import graphical_views.LoginView;
 
 public class ERP {
 	public static void main(String[] args) {
-		if (args[0].equals("Install")) {
-			DBInitiation dbi = new DBInitiation();
-			if (dbi.initiate())
-				System.out.println("Successfully Installed");
-			else 
-				System.out.println("Failed to Install");
-		}else if (args[0].equals("Run")){
+//		if (args[0].equals("Install")) {
+//			DBInitiation dbi = new DBInitiation();
+//			if (dbi.initiate())
+//				System.out.println("Successfully Installed");
+//			else 
+//				System.out.println("Failed to Install");
+//		}else if (args[0].equals("Run")){
 			LoginView login = new LoginView();
 			login.show(false);
-		}
+//		}
 	}
 }
